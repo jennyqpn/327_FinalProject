89cbf953dff315b0e6a6628cd1d919c4df181e20
Jenny Nguyen <jnguyen@CAPE.local>
2019-12-04 13:34:47 -0600

    food plays sound and disappears when picked up

---
+++ Unity3D_Basics_II/Assets/Scripts/food.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    AudioSource myaudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles += new Vector3(0, 2.0f, 0);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("player"))
+        {
+            StartCoroutine(PlayAndDestroy(myaudio.clip.length));
+        }
+    }
+
+    private IEnumerator PlayAndDestroy(float waitTime)
+    {
+        myaudio.Play();
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+}
